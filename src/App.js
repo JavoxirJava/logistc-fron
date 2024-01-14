871d45c0249d016c3107cbec7f2777391cb7c7d2
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-14 12:39:44 +0500

    salom
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Login from "./components/login/Login";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import Product from "./components/product/Product";
 import Dashboard from "./components/Dashboard";
@@ -7,11 +7,11 @@ import Dashboard from "./components/Dashboard";
 function App() {
     return (
         <div>
-            <NavBar/>
+            <NavBar />
+            <Dashboard />   
             <Routes>
                 {/* <Route path='/' element={<Login/>}/> */}
-                <Dashboard/>
-                <Route path='/product' element={<Product/>}/>
+                <Route path='/product' element={<Product />} />
             </Routes>
         </div>
     );
---
+++ src/components/Dashboard/statistics/cercle.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import EChartsReact from 'echarts-for-react';
+
+function Dashboard() {
+    const option = {
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            top: '5%',
+            left: 'center'
+        },
+        series: [
+            {
+                name: 'Access From',
+                type: 'pie',
+                radius: ['40%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    label: {
+                        show: true,
+                        fontSize: 40,
+                        fontWeight: 'bold'
+                    }
+                },
+                labelLine: {
+                    show: false
+                },
+                data: [
+                    { value: 1048, name: 'Search Engine' },
+                    { value: 735, name: 'Direct' },
+                    { value: 580, name: 'Email' },
+                    { value: 484, name: 'Union Ads' },
+                    { value: 300, name: 'Video Ads' }
+                ]
+            }
+        ]
+    };
+
+    return (
+        <div>
+            <div className="bg-white rounded-lg shadow-md p-5">
+                <div style={{ height: '400px', width: '100%', display: "flex", flexDirection: "column" }}>
+                    <EChartsReact
+                        option={option}
+                        style={{ height: '100%', width: '100%', display: "flex", justifyContent: "center" }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
