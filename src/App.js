47f692fee8bd27b48435ad13c0e6049c7e898acf
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-01-14 12:38:01 +0500

    w

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Login from "./components/login/Login";
 import {Route, Routes} from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import Product from "./components/product/Product";
+import Home from "./components/home page/Home";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <NavBar/>
             <Routes>
                 <Route path='/' element={<Login/>}/>
+                <Route path='/home' element={<Home/>}/>
                 <Route path='/product' element={<Product/>}/>
             </Routes>
         </div>
