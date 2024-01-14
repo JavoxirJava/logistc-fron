88e5d2bf80f0238f0c91272ca5d567e26cae3928
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-14 12:58:10 +0500

    aaaa

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -3,7 +3,7 @@ import LineChart from "./statistics/totop";
 
 function Dashboard() {
     return (
-        <div className="w-full flex flex-row">
+        <div className="w-full flex flex-row md:px-10 px-5 md:py-10 py-5">
             <div className="w-8/12">
                 <div className="h-[300px]">
                     <Circle />
