fe12429834c5bad4ed1fa066463e7d33bf95949b
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-14 16:19:06 +0500

    aaaaaa
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -3,8 +3,8 @@ import LineChart from "./statistics/totop";
 
 function Dashboard() {
     return (
-        <div className="w-full flex flex-row md:px-10 px-5 md:py-10 py-5">
-            <div className="w-8/12">
+        <div className="w-full flex lg:flex-row flex-col md:px-10 sd:px-5 px-2 md:py-10 py-5">
+            <div className="lg:w-8/12 w-full">
                 <div className="h-[320px] w-6/12">
                     <Circle />
                 </div>
@@ -12,7 +12,7 @@ function Dashboard() {
                     <LineChart />
                 </div>
             </div>
-            <div className="w-4/12 lg:ml-5">
+            <div className="lg:w-4/12 w-full lg:ml-5 lg:mt-0 mt-7">
                 <div className="mb-10 flex items-center">
                     <div className="flex justify-between w-full px-3">
                         <input
