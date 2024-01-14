af4da1018f6edc71b3429460b833da3e2fe685d3
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-14 12:22:39 +0500

    salom
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import './login.css';
-import TopSvg from "./TopSvg";
 
 function Login() {
     return (
-        <div className='login-main'>
-            <div className='elements'>
-                <TopSvg/>
-            </div>
-            <div className="box">
-                <p>Your login</p>
-                <h1>Login</h1>
-
+        <div className='w-full h-screen relative flex justify-center items-center header-div text-white'>
+            <div className="w-[450px] h-[470px] rounded-2xl box md:py-10 md:px-12 px-5 py-10 flex flex-col">
+                <p className='text-2xl mb-10'>Login</p>
+                <p>Phone Number</p>
+                <input type="text" className='input w-full rounded-md h-10 pl-3 mb-5' placeholder='Number'/>
+                <p>Password</p>
+                <input type="text" className='input w-full rounded-md h-10 pl-3' placeholder='Password'/>
+                <button className='button text-white w-full rounded-md h-10 mt-10'>Sign in</button>
             </div>
         </div>
     );
