e6493243aafdeddd78433e5d2983a78fb8d387ee
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-01-14 18:02:33 +0500

    salom

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,11 @@
 import kamaz from "./home/kamaz.png"
 import banner from "./home/banner.png"
 import kontener from "./home/kontener.png"
+import enter from "./home/enter.svg"
 
 export {
     kamaz,
     banner,
-    kontener
+    kontener,
+    enter
 }
