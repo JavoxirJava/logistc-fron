e279b5770e83efb2b5cda8a2620879b575909029
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-01-14 12:42:32 +0500

    clinet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Login from "./components/login/Login";
 import {Route, Routes} from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import Product from "./components/product/Product";
+import Clients from "./components/clients/Clients";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Login/>}/>
                 <Route path='/product' element={<Product/>}/>
+                <Route path='/client' element={<Clients/>}/>
             </Routes>
         </div>
     );
