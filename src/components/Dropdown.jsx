0580bff6055b1ccfedfd130b89ee45003ec22b05
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-15 12:32:30 +0500

    salom
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -13,14 +13,14 @@ const Dropdown = ({setSearchBy}) => {
         <div className="relative inline-block text-left float-end">
             <button
                 onClick={toggleDropdown} type="button"
-                className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="inline-flex justify-center w-full rounded sm:mt-0 mt-2 border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
                 Search by
             </button>
 
             {isOpen && (
                 <div
-                    className="origin-top-right absolute z-10 right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                    className="origin-top-right absolute z-10 right-0 mt-2 w-56 rounded shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                     <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                         <Link to="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" onClick={toggleDropdown}
                               role="menuitem">
