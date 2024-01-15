cc972e94855efd2c3448ecc575972dbc29e8f55c
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-01-15 16:05:26 +0500

    qazsdewwqqaazx

--- src/components/history/HistoryInfo.jsx
+++ src/components/history/HistoryInfo.jsx
@@ -1,40 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const HistoryInfo = ({ className, idNumber, address, status, etd, owner, product, detailId }) => {
-    return (
-        <div
-            className={`w-full ${className} history-info-bg border rounded-md border-slate-400 px-6 
-            py-4 text-center flex justify-center items-center`}>
-            <div className='w-[185px]'>
-                <p className='opacity-70'>Number</p>
-                <p className='font-bold text-[.9rem]'>{idNumber ? idNumber : 0}</p>
-            </div>
-            <div className='w-[185px]'>
-                <p className='opacity-70'>Current Location</p>
-                <p className='font-bold text-[.9rem]'>{address ? address : 'No location'}</p>
-            </div>
-            <div className='w-[185px]'>
-                <p className='opacity-70'>Status</p>
-                <p className='font-bold text-[.9rem] text-green-500'>{status ? status : 'Completed'}</p>
-            </div>
-            <div className='w-[185px]'>
-                <p className='opacity-70'>ETD</p>
-                <p className='font-bold text-[.9rem]'>{etd ? etd : 'April 23, 2022'}</p>
-            </div>
-            <div className='w-[185px]'>
-                <p className='opacity-70'>Owner</p>
-                <p className='font-bold text-[.9rem]'>{owner ? owner : 'No Owner'}</p>
-            </div>
-            <div className='w-[185px]'>
-                <p className='opacity-70'>Product</p>
-                <p className='font-bold text-[.9rem]'>{product ? product : 'No Product'}</p>
-            </div>
-            <div className='w-[190px]'>
-                <button className='py-2 px-6 bg-blue-800 border border-red-500 rounded-md 
-                text-white font-bold active:scale-95 duration-300 tracking-wide'>view details</button>
-            </div>
-        </div>
-    )
-}
+const HistoryInfo = ({ history }) => {
 
-export default HistoryInfo
+
+  return (
+    <>
+      {history.length &&
+        history.map((item, i) => (
+          <div
+          key={i}
+            className={`w-full ${item.className} history-info-bg border rounded-md border-slate-400 px-6 mt-2
+            py-4 text-center flex justify-center items-center`}
+          >
+            <div className="w-[185px]">
+              <p className="opacity-70">Number</p>
+              <p className="font-bold text-[.9rem]">
+                {item.idNumber ? item.idNumber : 0}
+              </p>
+            </div>
+            <div className="w-[185px]">
+              <p className="opacity-70">Current Location</p>
+              <p className="font-bold text-[.9rem]">
+                {item.address ? item.address : "No location"}
+              </p>
+            </div>
+            <div className="w-[185px]">
+              <p className="opacity-70">Status</p>
+              <p className="font-bold text-[.9rem] text-green-500">
+                {item.status ? item.status : "Completed"}
+              </p>
+            </div>
+            <div className="w-[185px]">
+              <p className="opacity-70">ETD</p>
+              <p className="font-bold text-[.9rem]">
+                {item.etd ? item.etd : "April 23, 2022"}
+              </p>
+            </div>
+            <div className="w-[185px]">
+              <p className="opacity-70">Owner</p>
+              <p className="font-bold text-[.9rem]">
+                {item.owner ? item.owner : "No Owner"}
+              </p>
+            </div>
+            <div className="w-[185px]">
+              <p className="opacity-70">Product</p>
+              <p className="font-bold text-[.9rem]">
+                {item.product ? item.product : "No Product"}
+              </p>
+            </div>
+            <div className="w-[190px]">
+              <button
+                className="py-2 px-6 bg-blue-800 border border-red-500 rounded-md 
+                text-white font-bold active:scale-95 duration-300 tracking-wide"
+              >
+                view details
+              </button>
+            </div>
+          </div>
+        ))}
+        
+    </>
+  );
+};
+
+export default HistoryInfo;
