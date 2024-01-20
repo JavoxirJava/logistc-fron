9e740b0dbea0afe9e1ecf5d3ef30901ceff85120
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-20 11:52:54 +0500

    salom
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/components/Offcanvas.jsx
+++ src/components/Offcanvas.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Offcanvas = ({isOpen, onClose, name, children}) => {
     return (
         <div
-            className={`fixed inset-0 overflow-hidden transition-opacity ${isOpen ? 'ease-out duration-300 opacity-100' : 'ease-in duration-200 opacity-0 pointer-events-none'}`}>
+            className={` fixed inset-0 overflow-hidden transition-opacity ${isOpen ? 'ease-out duration-300 opacity-100' : 'ease-in duration-200 opacity-0 pointer-events-none'}`}>
             <div className="absolute inset-0 overflow-hidden z-50">
                 <div
                     className="absolute inset-0 bg-opacity-75 transition-opacity"
@@ -11,8 +11,8 @@ const Offcanvas = ({isOpen, onClose, name, children}) => {
 
                 <section className="absolute inset-y-0 left-0 max-w-full flex outline-none">
                     <div className="w-screen max-w-lg">
-                        <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-auto w-[123%] col1 z-50">
-                            <div className="px-4 sm:px-6 pt-[10%]">
+                        <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-auto  col1 z-50">
+                            <div className="md:px-6 px-3 pt-[10%]">
                                 <h2 className="text-lg font-medium text-gray-900 inline-block">{name}</h2>
                                 <button type="button" onClick={onClose}
                                         className="float-end bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100">
@@ -25,7 +25,7 @@ const Offcanvas = ({isOpen, onClose, name, children}) => {
                                     </svg>
                                 </button>
                             </div>
-                            <div className="mt-6 relative flex-1 px-4 sm:px-6">
+                            <div className="mt-6 relative flex-1 md:px-6 sm:px-4 px-2">
                                 {children}
                             </div>
                         </div>
