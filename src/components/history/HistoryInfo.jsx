193a2f33327748533e4e95d8b422edba7aa1a87d
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-24 11:27:15 +0500

    salom
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/components/history/HistoryInfo.jsx
+++ src/components/history/HistoryInfo.jsx
@@ -20,49 +20,55 @@ const HistoryInfo = ({ history, className }) => {
                     className={`w-full ${className} history-info-bg border rounded-md border-slate-400 lg:px-6 px-2 mt-2
                     lg:py-4 py-2 text-center flex justify-center sm:items-center`}
                 >
-                    <div className="lg:w-[150px] md:w-[100px] sm:w-[80px] w-[50px]">
-                        <p className="opacity-70">{t("history6")}</p>
-                        <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]" >
-                            {item.productIdNumber ? item.productIdNumber : 0}
-                        </p>
+                    <div className="w-full flex sm:flex-row flex-col">
+                        <div className="sm:w-6/12 w-full flex">
+                            <div className="lg:w-[150px] ">
+                                <p className="opacity-70">{t("history6")}</p>
+                                <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]" >
+                                    {item.productIdNumber ? item.productIdNumber : 0}
+                                </p>
+                            </div>
+                            <div className="lg:w-[280px] ">
+                                <p className="opacity-70">{t("history7")}</p>
+                                <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]" >
+                                    {item.address ? item.address : "No location"}
+                                </p>
+                            </div>
+                            <div className="lg:w-[150px] ">
+                                <p className="opacity-70">{t("history8")}</p>
+                                <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]  text-green-500">
+                                    {item.status ? item.status : "Completed"}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="sm:w-6/12 w-full flex justify-between">
+                            <div className="lg:w-[130px] ">
+                                <p className="opacity-70">{t("history9")}</p>
+                                <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]" >
+                                    {item.date ? item.date.substring(0, 10) : "April 23, 2022"}
+                                </p>
+                            </div>
+                            <div className="lg:w-[130px] ">
+                                <p className="opacity-70">{t("history10")}</p>
+                                <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]" >
+                                    {item.owner ? item.owner : "No Owner"}
+                                </p>
+                            </div>
+                            <div className="lg:w-[130px] ">
+                                <p className="opacity-70">{t("history11")}</p>
+                                <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]" >
+                                    {item.productName ? item.productName : "No Product"}
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="lg:w-[300px] md:w-[170px] sm:w-[130px] w-[100px]">
-                        <p className="opacity-70">{t("history7")}</p>
-                        <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]" >
-                            {item.address ? item.address : "No location"}
-                        </p>
-                    </div>
-                    <div className="lg:w-[150px] md:w-[100px] sm:w-800px] w-[50px]">
-                        <p className="opacity-70">{t("history8")}</p>
-                        <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]  text-green-500">
-                            {item.status ? item.status : "Completed"}
-                        </p>
-                    </div>
-                    <div className="lg:w-[150px] md:w-[120px] sm:w-[100px] w-[50px]">
-                        <p className="opacity-70">{t("history9")}</p>
-                        <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]" >
-                            {item.date ? item.date.substring(0, 10) : "April 23, 2022"}
-                        </p>
-                    </div>
-                    <div className="lg:w-[150px] md:w-[120px] sm:w-[100px] w-[50px]">
-                        <p className="opacity-70">{t("history10")}</p>
-                        <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]" >
-                            {item.owner ? item.owner : "No Owner"}
-                        </p>
-                    </div>
-                    <div className="lg:w-[150px] md:w-[120px] sm:w-[100px] w-[50px]">
-                        <p className="opacity-70">{t("history11")}</p>
-                        <p className="font-bold lg:text-[.9rem] md:text-[.7rem] sm:text-[.5rem] text-[.5rem]" >
-                            {item.productName ? item.productName : "No Product"}
-                        </p>
-                    </div>
-                    <div className="w-[250px]">
+                    <div className="sm:w-[200px]">
                         <button
                             onClick={() => {
                                 setHistoryList(item);
                                 openModal();
                             }}
-                            className="py-2 px-6 bg-blue-800 border rounded-md
+                            className="sm:py-2 sm:px-6 px-2 py-1 bg-blue-800 border rounded-md
                             text-white font-bold active:scale-95 duration-300 tracking-wide"
                         >
                             {t("history5")}
