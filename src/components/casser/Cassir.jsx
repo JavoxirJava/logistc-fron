e76f91f6651660ea5daa332a64c4a18d30df399f
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-02-24 12:31:11 +0300

    cassier page ga api post yuli hamda get yullari ulandi

--- src/components/casser/Cassir.jsx
+++ src/components/casser/Cassir.jsx
@@ -1,10 +1,26 @@
 import NavBar from "../navbar/NavBar"
 import { useTranslation } from "react-i18next";
 import Modal from "./Modal";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { config, url } from "../api";
 
 
 const Cassir = ({ changeLanguage, lang }) => {
+    const [cassier, setCasseir] = useState(null)
     const { t } = useTranslation();
+
+    useEffect(() => {
+        getCassier();
+    }, [lang])
+
+    const getCassier = () => {
+        axios.get(`${url}cashier/all?lang=${lang}`, config)
+            .then(res => {
+                console.log(res.data);
+                // setCasseir(res.data)
+            })
+    }
 
     return (
         <div>
@@ -12,52 +28,67 @@ const Cassir = ({ changeLanguage, lang }) => {
             <div className="background h-screen pt-20  ">
                 <div className="md:w-[80vw] w-full mx-auto mt-[1.5rem] ">
                     <div className="flex flex-wrap justify-between">
-                        <Modal />
+                        <Modal getCassier={getCassier} />
                         <input
                             type="search"
                             placeholder="🔍Search"
                             className="outline-none pl-5 md:w-[40%] w-[80%] border-slate-100 border-2 rounded-md"
                         />
                     </div>
-                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
-                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
+                        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
-                                    <th scope="col" class="px-6 py-3">
+                                    <th className="px-6 py-3">
+                                        #
+                                    </th>
+                                    <th className="px-6 py-3">
                                         Product name
                                     </th>
-                                    <th scope="col" class="px-6 py-3">
+                                    <th className="px-6 py-3">
                                         Color
                                     </th>
-                                    <th scope="col" class="px-6 py-3">
+                                    <th className="px-6 py-3">
                                         Category
                                     </th>
-                                    <th scope="col" class="px-6 py-3">
+                                    <th className="px-6 py-3">
                                         Price
                                     </th>
-                                    <th scope="col" class="px-6 py-3">
+                                    <th className="px-6 py-3">
                                         Action
                                     </th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        Apple MacBook Pro 17"
-                                    </th>
-                                    <td class="px-6 py-4">
-                                        Silver
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        Laptop
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        $2999
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                    </td>
-                                </tr>
+                                {cassier ?
+                                    cassier.map((item, i) => (
+                                        <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                            <th className="px-6 py-3">
+                                                {i + 1}
+                                            </th>
+                                            <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                {item.name}
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                {item.status}
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                {item.tranport}
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                {item.totalKub}
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                            </td>
+                                        </tr>
+                                    )) : (
+                                        <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                            <td colSpan='6' className="text-center py-3">
+                                                dfguyasergfiwa
+                                            </td>
+                                        </tr>
+                                    )}
                             </tbody>
                         </table>
                     </div>
