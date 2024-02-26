2a6ef9b6ac34c0637f65a4078f057caa8ffbffc0
Ozodbek <ozodbek007@gmail.com>
2024-02-26 22:45:33 +0500

    AddProjectInfoModal.jsx

--- src/components/werHouse/ViewMore.jsx
+++ src/components/werHouse/ViewMore.jsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import NavBar from "../navbar/NavBar";
 import axios from "axios";
-import { config, getFile, url } from "../api";
+import {config, getFile, url} from "../api";
 import ReactPaginate from "react-paginate";
 import img from "../empty.png";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 import OffcanvasProduct from "./OffcanvasProduct";
-import { toast } from "react-toastify";
+import {toast} from "react-toastify";
 import ProductDModal from "./productModl";
+import AddProjectInfoModal from "./AddProjectInfoModal";
 
-const ViewMoreW = ({ lang }) => {
+const ViewMoreW = ({lang}) => {
     const [product2, setProductObj2] = useState(null);
     const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(false);
     const [projectIdInfo, setProjectIdInfo] = useState(null);
@@ -21,7 +22,8 @@ const ViewMoreW = ({ lang }) => {
     const [isModalDelete, setIsDeleteOpen] = useState(false);
     const [ModalPro, setProOpen] = useState(false);
     const [currentPage, setCurrentPage] = useState(0);
-    const [productIds, setProductIds] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [showModal, setShowModal] = useState(false);
 
     let projectId = sessionStorage.getItem("warehouseIdViewMore");
 
@@ -32,8 +34,9 @@ const ViewMoreW = ({ lang }) => {
     const closeDelete = () => setIsDeleteOpen(false);
     const openPro = () => setProOpen(true);
     const closePro = () => setProOpen(false);
+    const showProjectInfoModal = () => setShowModal(!showModal);
 
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
     useEffect(() => {
         getProject();
@@ -81,17 +84,14 @@ const ViewMoreW = ({ lang }) => {
         let data = e.target.value;
         console.log(!!data);
         if (!!data) {
-            axios
-                .get(
+            axios.get(
                     `${url}wareHouse/product/search?userName=${data}&lang=${lang}`,
                     config
-                )
-                .then((res) =>
+                ).then((res) =>
                     res.data.success === false
-                        ? setProjectIdInfo([{ comment: "Not found 😊" }])
+                        ? setProjectIdInfo([{comment: "Not found 😊"}])
                         : setProjectIdInfo(res.data.body)
-                )
-                .catch(() => setProjectIdInfo(null));
+                ).catch(() => setProjectIdInfo(null));
         } else {
             getProjectInfo();
             setProjectIdInfo(null);
@@ -99,80 +99,70 @@ const ViewMoreW = ({ lang }) => {
     };
 
     function addProduct() {
-        let data = { ...product2 };
+        let data = {...product2};
         axios.post(`${url}product?userId=${userId}`, data, config)
             .then(() => {
                 toast.success(t("success"));
                 getProjectInfo();
                 setProductObj2(null);
-            })
-            .catch((err) => {
+            }).catch((err) => {
                 toast.error(t("error"));
                 console.log(err);
             });
     }
 
     function addtoProduct() {
-        let data = [...productIds];
+        let data = [...products.map(p => p.productId)];
 
         let datas = {
             productIds: data,
             projectId: document.getElementById("projects").value,
             wareHouseId: projectId,
         };
-        axios
-            .post(`${url}product/change-warehouse-to-project`, datas, config)
-            .then(() => {
-                toast.success(t("success"));
-                getProjectInfo();
-                setProductObj2(null);
-            })
-            .catch((err) => {
-                toast.error(t("error"));
-                console.log(err);
-            });
+        axios.post(`${url}product/change-warehouse-to-project`, datas, config).then(() => {
+            toast.success(t("success"));
+            getProjectInfo();
+            setProductObj2(null);
+        }).catch((err) => {
+            toast.error(t("error"));
+            console.log(err);
+        });
     }
 
     function editProduct() {
-        let data = { ...product2 };
-        axios
-            .put(`${url}product?id=${product.productId}`, data, config)
+        let data = {...product2};
+        axios.put(`${url}product?id=${product.productId}`, data, config)
             .then(() => {
                 toast.success(t("success"));
                 getProjectInfo();
                 setProductObj2(null);
-            })
-            .catch((err) => {
-                toast.error(t("error"));
-                console.log(err);
-            });
+            }).catch((err) => {
+            toast.error(t("error"));
+            console.log(err);
+        });
     }
 
     function deleteProduct() {
-        axios
-            .delete(
-                `${url}product/ware-house?wareHouseId=${projectId}&productId=${product.productId}`,
-                config
-            )
-            .then(() => {
-                getProjectInfo();
-                toast.success(t("success"));
-            })
-            .catch((err) => {
-                toast.error(t("error"));
-                // console.log(err);
-            });
+        axios.delete(
+            `${url}product/ware-house?wareHouseId=${projectId}&productId=${product.productId}`,
+            config
+        ).then(() => {
+            getProjectInfo();
+            toast.success(t("success"));
+        }).catch(() => {
+            toast.error(t("error"));
+            // console.log(err);
+        });
     }
 
-    function addProductIds(checked, id, item) {
-        console.log(item);
-        if (checked) setProductIds([...productIds, id]);
-        else setProductIds(productIds.filter((productId) => productId !== id));
+    function addProductIds(checked, item) {
+        if (checked) setProducts([...products, item]);
+        else setProducts(products.filter((product) => product.productId !== item.productId));
     }
 
     return (
         <div className="w-full h-screen background overflow-x-hidden ">
-            <NavBar lang={lang} />
+            <NavBar lang={lang}/>
             <div className="mt-32 flex justify-start md:ml-20 ml-0  w-full">
                 <input
                     type="search"
@@ -206,86 +196,86 @@ const ViewMoreW = ({ lang }) => {
                 <div className="lg:px-0 md:px-10 lg:py-0 sm:py-5 px-3 mt-8 w-screen lg:w-[90%] overflow-x-auto">
                     <table className="w-full rounded-2xl overflow-hidden text-gray-500">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-                            <tr>
-                                <th className="px-6 py-3">№</th>
-                                <th className="px-6 py-3">{t("photo")}</th>
-                                <th className="px-6 py-3">{t("owner")}</th>
-                                <th className="px-6 py-3">{t("productName")}</th>
-                                <th className="px-6 py-3">{t("productAdd34")}</th>
-                                <th className="px-6 py-3">{t("date")}</th>
-                                <th className="px-6 py-3">{t("productCount")}</th>
-                                <th className="px-6 py-3">{t("totalWeight")}</th>
-                                <th className="px-6 py-3">{t("totalKub")}</th>
-                                <th className="px-6 py-3">{t("edit")}</th>
-                                <th className="px-6 py-3">{t("delete")}</th>
-                                <th className="px-6 py-3">{t("selectCh")}</th>
-                            </tr>
+                        <tr>
+                            <th className="px-6 py-3">№</th>
+                            <th className="px-6 py-3">{t("photo")}</th>
+                            <th className="px-6 py-3">{t("owner")}</th>
+                            <th className="px-6 py-3">{t("productName")}</th>
+                            <th className="px-6 py-3">{t("productAdd34")}</th>
+                            <th className="px-6 py-3">{t("date")}</th>
+                            <th className="px-6 py-3">{t("productCount")}</th>
+                            <th className="px-6 py-3">{t("totalWeight")}</th>
+                            <th className="px-6 py-3">{t("totalKub")}</th>
+                            <th className="px-6 py-3">{t("edit")}</th>
+                            <th className="px-6 py-3">{t("delete")}</th>
+                            <th className="px-6 py-3">{t("selectCh")}</th>
+                        </tr>
                         </thead>
                         <tbody className="text-[1rem] text-gray-700 bg-white text-center">
-                            {projectIdInfo ? (
-                                projectIdInfo.map((item, i) => (
-                                    <tr className="border-b transition duration-300 ease-in-out hover:bg-gray-300">
-                                        <th className="px-6 py-5">{currentPage * 10 + (i + 1)}</th>
-                                        <th className="px-6 py-5 flex justify-center items-center">
-                                            <img
-                                                src={
-                                                    item.attachmentId ? getFile + item.attachmentId : img
-                                                }
-                                                className="w-10 h-10 object-cover rounded-full scale-150"
-                                                alt="img"
-                                            />
-                                        </th>
-                                        <td className="px-6 py-5">{item.owner}</td>
-                                        <td className="px-6 py-5">{item.productName}</td>
-                                        <td className="px-6 py-5">{item.comment}</td>
-                                        <td className="px-6 py-5 overflow-x-auto">
-                                            {item.date.slice(0, item.date.indexOf(" "))}
-                                        </td>
-                                        <td className="px-6 py-5">{item.productCount}</td>
-                                        <td className="px-6 py-5">{item.totalWeight}</td>
-                                        <td className="px-6 py-5">{item.totalKub}</td>
-                                        <td className="px-6 py-4">
-                                            <a
-                                                onClick={() => {
-                                                    openEdit();
-                                                    setProductObj(item);
-                                                }}
-                                                href="#"
-                                                className="font-medium text-[#16A34A] hover:underline"
-                                            >
-                                                {t("edit")}
-                                            </a>
-                                        </td>
-                                        <td className="px-6 py-4">
-                                            <a
-                                                onClick={() => {
-                                                    setProductObj(item);
-                                                    openDelete();
-                                                }}
-                                                href="#"
-                                                className="font-medium text-[#c0332e] hover:underline"
-                                            >
-                                                {t("delete")}
-                                            </a>
-                                        </td>
-                                        <td className="px-6 py-4">
-                                            <input
-                                                onChange={(e) =>
-                                                    addProductIds(e.target.checked, item.productId, item)
-                                                }
-                                                type="checkbox"
-                                                className="w-5 h-5"
-                                            />
-                                        </td>
-                                    </tr>
-                                ))
-                            ) : (
-                                <tr>
-                                    <td colSpan="12" className="px-4">
-                                        <h1 className="text-xl text-black">{t("notfound")}</h1>
+                        {projectIdInfo ? (
+                            projectIdInfo.map((item, i) => (
+                                <tr key={i} className="border-b transition duration-300 ease-in-out hover:bg-gray-300">
+                                    <th className="px-6 py-5">{currentPage * 10 + (i + 1)}</th>
+                                    <th className="px-6 py-5 flex justify-center items-center">
+                                        <img
+                                            src={
+                                                item.attachmentId ? getFile + item.attachmentId : img
+                                            }
+                                            className="w-10 h-10 object-cover rounded-full scale-150"
+                                            alt="img"
+                                        />
+                                    </th>
+                                    <td className="px-6 py-5">{item.owner}</td>
+                                    <td className="px-6 py-5">{item.productName}</td>
+                                    <td className="px-6 py-5">{item.comment}</td>
+                                    <td className="px-6 py-5 overflow-x-auto">
+                                        {item.date.slice(0, item.date.indexOf(" "))}
+                                    </td>
+                                    <td className="px-6 py-5">{item.productCount}</td>
+                                    <td className="px-6 py-5">{item.totalWeight}</td>
+                                    <td className="px-6 py-5">{item.totalKub}</td>
+                                    <td className="px-6 py-4">
+                                        <a
+                                            onClick={() => {
+                                                openEdit();
+                                                setProductObj(item);
+                                            }}
+                                            href="#"
+                                            className="font-medium text-[#16A34A] hover:underline"
+                                        >
+                                            {t("edit")}
+                                        </a>
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        <a
+                                            onClick={() => {
+                                                setProductObj(item);
+                                                openDelete();
+                                            }}
+                                            href="#"
+                                            className="font-medium text-[#c0332e] hover:underline"
+                                        >
+                                            {t("delete")}
+                                        </a>
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        <input
+                                            onChange={(e) =>
+                                                addProductIds(e.target.checked, item)
+                                            }
+                                            type="checkbox"
+                                            className="w-5 h-5"
+                                        />
                                     </td>
                                 </tr>
-                            )}
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan="12" className="px-4">
+                                    <h1 className="text-xl text-black">{t("notfound")}</h1>
+                                </td>
+                            </tr>
+                        )}
                         </tbody>
                     </table>
                     <div className="mt-7 ms-1 mb-20 lg:mb-10">
@@ -344,7 +334,7 @@ const ViewMoreW = ({ lang }) => {
 
             <div
                 className={`fixed ${ModalPro ? "block" : "hidden"
-                    } inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full`}
+                } inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full`}
             >
                 <div className="relative top-20 mx-auto p-5 border md:w-96 w-[200px] shadow-lg rounded-md bg-white">
                     <div>
@@ -354,16 +344,10 @@ const ViewMoreW = ({ lang }) => {
                                 id={`projects`}
                                 className="block w-full  p-2 border rounded-md shadow-sm focus:outline-0 mb-4"
                             >
-                                <option selected disabled>
-                                    {t("select")}
-                                </option>
+                                <option selected disabled>{t("select")}</option>
 
                                 {project &&
-                                    project.map((item, i) =>
-                                        <option key={i} value={item.id}>
-                                            {item.name}
-                                        </option>
-                                    )}
+                                    project.map((item, i) => <option key={i} value={item.id}>{item.name}</option>)}
                             </select>
                         </div>
                         <div className="flex justify-between mt-7">
@@ -373,7 +357,7 @@ const ViewMoreW = ({ lang }) => {
                             <button
                                 onClick={() => {
                                     closePro();
-                                    addtoProduct();
+                                    showProjectInfoModal();
                                 }}
                                 className="btmn"
                             >
@@ -383,6 +367,12 @@ const ViewMoreW = ({ lang }) => {
                     </div>
                 </div>
             </div>
+
+            {/*    Modal */}
+
+            {showModal ? (
+                <AddProjectInfoModal showProjectInfoModal={showProjectInfoModal} products={products} addToProduct={addtoProduct}/>
+            ) : ''}
         </div>
     );
 };
