45c1845370d9aa4c9b4df826bae62024abfa318f
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-02-26 18:16:30 +0300

    Sardorbek Werhouseni table ni tugatdi hamda cassir ni tablega not found page qushildi

--- src/components/werHouse/ViewMore.jsx
+++ src/components/werHouse/ViewMore.jsx
@@ -1,15 +1,15 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import NavBar from "../navbar/NavBar";
 import axios from "axios";
-import {config, getFile, url} from "../api";
+import { config, getFile, url } from "../api";
 import ReactPaginate from "react-paginate";
 import img from "../empty.png";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 import OffcanvasProduct from "./OffcanvasProduct";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 import ProductDModal from "./productModl";
 
-const ViewMoreW = ({lang}) => {
+const ViewMoreW = ({ lang }) => {
     const [product2, setProductObj2] = useState(null);
     const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(false);
     const [projectIdInfo, setProjectIdInfo] = useState(null);
@@ -33,7 +33,7 @@ const ViewMoreW = ({lang}) => {
     const openPro = () => setProOpen(true);
     const closePro = () => setProOpen(false);
 
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
     useEffect(() => {
         getProject();
@@ -87,7 +87,7 @@ const ViewMoreW = ({lang}) => {
                 )
                 .then((res) =>
                     res.data.success === false
-                        ? setProjectIdInfo([{comment: "Not found 😊"}])
+                        ? setProjectIdInfo([{ comment: "Not found 😊" }])
                         : setProjectIdInfo(res.data.body)
                 )
                 .catch(() => setProjectIdInfo(null));
@@ -98,7 +98,7 @@ const ViewMoreW = ({lang}) => {
     };
 
     function addProduct() {
-        let data = {...product2};
+        let data = { ...product2 };
         axios.post(`${url}product?userId=${userId}`, data, config)
             .then(() => {
                 toast.success(t("success"));
@@ -133,7 +133,7 @@ const ViewMoreW = ({lang}) => {
     }
 
     function editProduct() {
-        let data = {...product2};
+        let data = { ...product2 };
         axios
             .put(`${url}product?id=${product.productId}`, data, config)
             .then(() => {
@@ -170,31 +170,24 @@ const ViewMoreW = ({lang}) => {
 
     return (
         <div className="w-full h-screen background overflow-x-hidden ">
-            <NavBar lang={lang}/>
-            <div className="mt-32 flex justify-center w-full">
+            <NavBar lang={lang} />
+            <div className="mt-32 flex justify-start md:ml-20 ml-0  w-full">
                 <input
                     type="search"
                     onChange={searchHandler}
                     placeholder={t("productSearch")}
-                    className="lg:w-8/12 px-4 h-10 focus:outline-0 border rounded-md"
+                    className="lg:w-5/12 w-[100%] md:mx-0 mx-3 px-4 h-10 focus:outline-0 border rounded-md"
                 />
             </div>
-            <div className="flex w-full justify-center">
-                <div className="flex w-10/12 justify-between my-5">
-                    <button
-                        onClick={handleToggleOffcanvas}
-                        className="bg-green-600 hover:bg-green-700 text-white font-bold py-1.5 px-8  rounded"
-                    >
-                        {t("productAdd1")}
-                    </button>
-                    <h1>
-                        <b>{t("werhouse")}</b>
-                    </h1>
-
-                    {/* <span className="me-5 pt-1.5 float-end">
-                {t("cardCurrent")}: {pagination2}
-              </span> */}
-                    <div className="flex">
+            <div className="flex w-full justify-center ">
+                <div className="flex md:w-[89%] w-full md:flex-row flex-col justify-between mt-5">
+                    <div className="flex md:justify-start justify-center gap-5">
+                        <button
+                            onClick={handleToggleOffcanvas}
+                            className="bg-green-600 hover:bg-green-700 text-white font-bold py-1.5 px-8  rounded"
+                        >
+                            {t("productAdd1")}
+                        </button>
                         <button
                             onClick={openPro}
                             className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-1.5 px-8  rounded"
@@ -202,98 +195,102 @@ const ViewMoreW = ({lang}) => {
                             {t("addproject")}
                         </button>
                     </div>
+                    <h1 className="md:ml-0 ml-5">
+                        <b>{t("werhouse")}</b>
+                    </h1>
                 </div>
             </div>
             <div className="product-main flex justify-center items-start overflow-hidden w-full">
                 <div className="lg:px-0 md:px-10 lg:py-0 sm:py-5 px-3 mt-8 w-screen lg:w-[90%] overflow-x-auto">
                     <table className="w-full rounded-2xl overflow-hidden text-gray-500">
-                        <thead className="text-[1.1rem] text-slate-50 uppercase bg-slate-500">
-                        <tr>
-                            <th className="px-6 py-3">№</th>
-                            <th className="px-6 py-3">{t("photo")}</th>
-                            <th className="px-6 py-3">{t("owner")}</th>
-                            <th className="px-6 py-3">{t("productName")}</th>
-                            <th className="px-6 py-3">{t("productAdd34")}</th>
-                            <th className="px-6 py-3">{t("date")}</th>
-                            <th className="px-6 py-3">{t("productCount")}</th>
-                            <th className="px-6 py-3">{t("totalWeight")}</th>
-                            <th className="px-6 py-3">{t("totalKub")}</th>
-                            <th className="px-6 py-3">{t("edit")}</th>
-                            <th className="px-6 py-3">{t("delete")}</th>
-                            <th className="px-6 py-3">{t("selectCh")}</th>
-                        </tr>
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                            <tr>
+                                <th className="px-6 py-3">№</th>
+                                <th className="px-6 py-3">{t("photo")}</th>
+                                <th className="px-6 py-3">{t("owner")}</th>
+                                <th className="px-6 py-3">{t("productName")}</th>
+                                <th className="px-6 py-3">{t("productAdd34")}</th>
+                                <th className="px-6 py-3">{t("date")}</th>
+                                <th className="px-6 py-3">{t("productCount")}</th>
+                                <th className="px-6 py-3">{t("totalWeight")}</th>
+                                <th className="px-6 py-3">{t("totalKub")}</th>
+                                <th className="px-6 py-3">{t("edit")}</th>
+                                <th className="px-6 py-3">{t("delete")}</th>
+                                <th className="px-6 py-3">{t("selectCh")}</th>
+                            </tr>
                         </thead>
                         <tbody className="text-[1rem] text-gray-700 bg-white text-center">
-                        {projectIdInfo ? (
-                            projectIdInfo.map((item, i) => (
-                                <tr className="border-b transition duration-300 ease-in-out hover:bg-gray-300">
-                                    <th className="px-6 py-5">{currentPage * 10 + (i + 1)}</th>
-                                    <th className="px-6 py-5 flex justify-center items-center">
-                                        <img
-                                            src={
-                                                item.attachmentId ? getFile + item.attachmentId : img
-                                            }
-                                            className="w-10 h-10 object-cover rounded-full scale-150"
-                                            alt="img"
-                                        />
-                                    </th>
-                                    <td className="px-6 py-5">{item.owner}</td>
-                                    <td className="px-6 py-5">{item.productName}</td>
-                                    <td className="px-6 py-5">{item.comment}</td>
-                                    <td className="px-6 py-5 overflow-x-auto">
-                                        {item.date.slice(0, item.date.indexOf(" "))}
-                                    </td>
-                                    <td className="px-6 py-5">{item.productCount}</td>
-                                    <td className="px-6 py-5">{item.totalWeight}</td>
-                                    <td className="px-6 py-5">{item.totalKub}</td>
-                                    <td className="px-6 py-4">
-                                        <a
-                                            onClick={() => {
-                                                openEdit();
-                                                setProductObj(item);
-                                            }}
-                                            href="#"
-                                            className="font-medium text-[#16A34A] hover:underline"
-                                        >
-                                            {t("edit")}
-                                        </a>
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        <a
-                                            onClick={() => {
-                                                setProductObj(item);
-                                                openDelete();
-                                            }}
-                                            href="#"
-                                            className="font-medium text-[#c0332e] hover:underline"
-                                        >
-                                            {t("delete")}
-                                        </a>
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        <input
-                                            onChange={(e) =>
-                                                addProductIds(e.target.checked, item.productId)
-                                            }
-                                            type="checkbox"
-                                            className="w-5 h-5"
-                                        />
+                            {projectIdInfo ? (
+                                projectIdInfo.map((item, i) => (
+                                    <tr className="border-b transition duration-300 ease-in-out hover:bg-gray-300">
+                                        <th className="px-6 py-5">{currentPage * 10 + (i + 1)}</th>
+                                        <th className="px-6 py-5 flex justify-center items-center">
+                                            <img
+                                                src={
+                                                    item.attachmentId ? getFile + item.attachmentId : img
+                                                }
+                                                className="w-10 h-10 object-cover rounded-full scale-150"
+                                                alt="img"
+                                            />
+                                        </th>
+                                        <td className="px-6 py-5">{item.owner}</td>
+                                        <td className="px-6 py-5">{item.productName}</td>
+                                        <td className="px-6 py-5">{item.comment}</td>
+                                        <td className="px-6 py-5 overflow-x-auto">
+                                            {item.date.slice(0, item.date.indexOf(" "))}
+                                        </td>
+                                        <td className="px-6 py-5">{item.productCount}</td>
+                                        <td className="px-6 py-5">{item.totalWeight}</td>
+                                        <td className="px-6 py-5">{item.totalKub}</td>
+                                        <td className="px-6 py-4">
+                                            <a
+                                                onClick={() => {
+                                                    openEdit();
+                                                    setProductObj(item);
+                                                }}
+                                                href="#"
+                                                className="font-medium text-[#16A34A] hover:underline"
+                                            >
+                                                {t("edit")}
+                                            </a>
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <a
+                                                onClick={() => {
+                                                    setProductObj(item);
+                                                    openDelete();
+                                                }}
+                                                href="#"
+                                                className="font-medium text-[#c0332e] hover:underline"
+                                            >
+                                                {t("delete")}
+                                            </a>
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <input
+                                                onChange={(e) =>
+                                                    addProductIds(e.target.checked, item.productId)
+                                                }
+                                                type="checkbox"
+                                                className="w-5 h-5"
+                                            />
+                                        </td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="12" className="px-4">
+                                        {/* <div className={styles.loading}></div>
+                                        <div className={styles.loading}></div>
+                                        <div className={styles.loading}></div>
+                                        <div className={styles.loading}></div>
+                                        <div className={styles.loading}></div>
+                                        <div className={styles.loading}></div>
+                                        <div className={styles.loading}></div> */}
+                                        <h1 className="text-xl text-black">Not Found</h1>
                                     </td>
                                 </tr>
-                            ))
-                        ) : (
-                            <tr>
-                                <td colSpan="12" className="px-4">
-                                    <div className={styles.loading}></div>
-                                    <div className={styles.loading}></div>
-                                    <div className={styles.loading}></div>
-                                    <div className={styles.loading}></div>
-                                    <div className={styles.loading}></div>
-                                    <div className={styles.loading}></div>
-                                    <div className={styles.loading}></div>
-                                </td>
-                            </tr>
-                        )}
+                            )}
                         </tbody>
                     </table>
                     <div className="mt-7 ms-1 mb-20 lg:mb-10">
@@ -351,14 +348,13 @@ const ViewMoreW = ({lang}) => {
             />
 
             <div
-                className={`fixed ${
-                    ModalPro ? "block" : "hidden"
-                } inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full`}
+                className={`fixed ${ModalPro ? "block" : "hidden"
+                    } inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full`}
             >
                 <div className="relative top-20 mx-auto p-5 border md:w-96 w-[200px] shadow-lg rounded-md bg-white">
                     <div>
                         <div>
-                          <label htmlFor="projects">{t("select")}</label>
+                            <label htmlFor="projects">{t("select")}</label>
                             <select
                                 id={`projects`}
                                 className="block w-full  p-2 border rounded-md shadow-sm focus:outline-0 mb-4"
@@ -366,9 +362,9 @@ const ViewMoreW = ({lang}) => {
                                 <option selected disabled>
                                     {t("select")}
                                 </option>
-                                
+
                                 {project &&
-                                    project.map((item, i) => 
+                                    project.map((item, i) =>
                                         <option key={i} value={item.id}>
                                             {item.name}
                                         </option>
