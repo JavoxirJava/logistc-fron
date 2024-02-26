15e2889d0371e6f318439cbeca1686d5a2bec26b
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-02-26 18:32:10 +0500

    sdvs

--- src/components/Dashboard/card/index.jsx
+++ src/components/Dashboard/card/index.jsx
@@ -141,7 +141,7 @@ const DashboardProductCard = ({ className, lang }) => {
               placeholder="🔍..."
               defaultValue=""
               onChange={searchProduct}
-              className="lg:w-10/12 ps-2 h-10 focus:outline-0 border sm:mt-0 mt-2"
+              className="lg:w-7/12 ps-2 h-10 focus:outline-0 border sm:mt-0 mt-2"
             />
           )}
         </div>
