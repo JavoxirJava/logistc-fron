85043be1ecc9c420cb128bf726f1cd1b8a74c31e
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-02-28 11:52:16 +0500

    sdf

--- src/components/Dashboard/statistics/linestatistic/index.jsx
+++ src/components/Dashboard/statistics/linestatistic/index.jsx
@@ -3,45 +3,86 @@ import "../../index.css"
 import EChartsReact from 'echarts-for-react';
 
 function LineChart({ productStatistics2 }) {
+  console.log(productStatistics2);
+  const flattenedData = productStatistics2.flatMap(item => item.map(innerItem => innerItem));
+
   const option = {
     xAxis: {
       type: 'category',
-      data: productStatistics2.map(product => product.month)
+      data: flattenedData.map(item => item.month)
     },
     yAxis: {
       type: 'value'
     },
     tooltip: {
-      trigger: 'axis',
-      formatter: function (params) {
-        let tooltip = params[0].axisValueLabel + '<br/>';
-        params.forEach(param => {
-          tooltip += param.data.status + ': ' + param.data.result + "<br/>";
-        });
-        return tooltip;
-      }
+      trigger: 'axis'
     },
     legend: {
-      data: Object.keys(productStatistics2[0])
-        .filter(key => key !== 'month' && key !== 'year' && key !== 'monthNumber')
+      data: flattenedData.reduce((legendData, item) => {
+        const keys = Object.keys(item);
+        const statusKeys = keys.filter(key => key !== 'month' && key !== 'year' && key !== 'monthNumber');
+        statusKeys.forEach(statusKey => {
+          if (item[statusKey] && item[statusKey].status && !legendData.includes(item[statusKey].status)) {
+            legendData.push(item[statusKey].status);
+          }
+        });
+        return legendData;
+      }, [])
     },
     series: []
   };
 
-  if (productStatistics2.length > 0) {
-    const seriesData = Object.keys(productStatistics2[0])
+  if (flattenedData.length > 0) {
+    const seriesData = Object.keys(flattenedData[0])
       .filter(key => key !== 'month' && key !== 'year' && key !== 'monthNumber')
       .map(key => ({
-        name: productStatistics2[0][key].status,
+        name: flattenedData[0][key] && flattenedData[0][key].status ? flattenedData[0][key].status : '',
         type: 'bar',
-        data: productStatistics2.map(product => ({
-          value: product[key].result,
-          status: product[key].status,
-          result: product[key].result
-        }))
+        data: flattenedData.map(item => item[key] && item[key].projectId ? item[key].projectId : 0) // Use 0 if projectId is undefined
       }));
     option.series = seriesData;
   }
+
+
+  // [
+  //   {
+  //     "data-1": []
+  //   }
+  // ],
+  // [
+  //   {
+  //     "data-2": []
+  //   }
+  // ],
+  // [
+  //   {
+  //     "data-3": []
+  //   }
+  // ],
+  // [
+  //   {
+  //     "data-4": []
+  //   }
+  // ],
+  // [
+  //   {
+  //     "data-5": []
+  //   }
+  // ],
+  // [
+  //   {
+  //     "data-6": [
+  //       {
+  //         "month": "February",
+  //         "year": 2024,
+  //         "monthNumber": 2,
+  //         "projectName": "Project",
+  //         "projectId": 1,
+  //         "status": "Ready"
+  //       }
+  //     ]
+  //   }
+  // ]
 
   return (
     <div className=''>
