6e66b41922487a2a5e37334ce1dae0d0cd6a805e
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-03-07 18:30:20 +0500

    user table xam buldi

--- src/components/user/Dashboard/card/index.jsx
+++ src/components/user/Dashboard/card/index.jsx
@@ -2,61 +2,23 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { config, getFile, url } from "../../../api";
-import Dropdown from "./Dropdown";
 import "../../../product/product.css";
 import img from "../../../empty.png";
 import Pagination, {
   bootstrap5PaginationPreset,
 } from "react-responsive-pagination";
 import { useTranslation } from "react-i18next";
-import { changeLanguage } from "i18next";
 import { toast } from "react-toastify";
 import ImageViewModal from "../../../ImageViewModal";
 
-const Modal = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
-
-  return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75 z-10">
-      <div className="bg-white p-8 rounded shadow-md w-[500px]">
-        <div className="bg-blue-800 flex justify-around items-center relative">
-          <h1 className="text-2xl text-sky-200">Phone</h1>
-          <h1 className="text-2xl text-green-300">Going</h1>
-          <i
-            className="fa-solid fa-xmark absolute right-2 text-2xl text-white"
-            onClick={onClose}
-          ></i>
-        </div>
-        <div className="bg-sky-200 px-20 py-6 flex justify-between">
-          <div className=" ">
-            <p className="pb-0 mb-0 mt-2">Name</p>
-            <p className="mt-0 pt-0">John Doe</p>
-          </div>
-          <div>
-            <p className="pb-0 mb-0 mt-2">Name</p>
-            <p className="mt-0 pt-0">John Doe</p>
-          </div>
-        </div>
-        <div></div>
-      </div>
-    </div>
-  );
-};
-
-const DashboardProductCard = ({ className, lang }) => {
+const DashboardProductCard = ({ lang }) => {
   const [modal, setModal] = useState(false);
   const [modalin, setModalin] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [data, setProduct] = useState(null);
   const [totalPage, setTotalPage] = useState(1);
   const [pagination, setPagination] = useState(0);
-  const [searchBy, setSearchBy] = useState(null);
-  const [drops, setDrops] = useState(false);
   const [isImageOpenModal, setIsImageOpenModal] = useState(false);
   const [imageId, setImageId] = useState(false);
-
-  const inputDrop = () => setDrops(false);
-  const selectDrop = () => setDrops(true);
 
   useEffect(() => {
     getProduct(pagination, 4);
@@ -74,8 +36,8 @@ const DashboardProductCard = ({ className, lang }) => {
   }, [pagination]);
 
 
-  const openModal = () => {setModal(true)}
-  const closeModal = () => {setModal(false)}
+  const openModal = () => { setModal(true) }
+  const closeModal = () => { setModal(false) }
 
   function getProduct(page, size) {
     axios
@@ -91,10 +53,16 @@ const DashboardProductCard = ({ className, lang }) => {
   }
 
   function getInfo(id) {
-    axios
-      .get(`${url}cashier/user?productId=${id}&lang=${lang}`, config)
+    axios.get(`${url}cashier/user?productId=${id}&lang=${lang}`, config)
       .then((res) => {
-        setModalin(res.data.body);
+        if (res.data.body.active === true) {
+          setModalin(res.data.body)
+          openModal()
+        }
+        else {
+          setModalin(null)
+          toast.warning('tasdiqlanmagan')
+        }
       })
       .catch((err) => console.log(err));
   }
@@ -120,51 +88,17 @@ const DashboardProductCard = ({ className, lang }) => {
         .catch((err) => console.log(err));
   }
 
-  function searchByName() {
-    switch (searchBy) {
-      case "Project name":
-        return "name";
-      case "Project status":
-        return "status";
-      default:
-        return "name";
-    }
-  }
-
-
   return (
     <div className="radius">
       <div className="mb-5 flex items-center">
         <div className="flex justify-between items-center w-full md:px-3 ">
-          {drops ? (
-            <select
-              onChange={searchProduct}
-              defaultValue=""
-              id="statuslar"
-              className="py-2 px-2 w-96 bg-white rounded-lg  border border-slate-300
-                       focus:outline-0 focus:border-slate-500 duration-300 focus:bg-slate-100 shadow-md
-                     focus:placeholder:text-slate-800 placeholder:duration-300 placeholder:font-medium"
-            >
-              <option selected disabled>
-                {t("productAdd60")}
-              </option>
-              <option value="all">{t("all")}</option>
-              <option value="PENDING">{t("status1")}</option>
-              <option value="GOING">{t("status2")}</option>
-              <option value="CANCEL">{t("status3")}</option>
-              <option value="ARRIVED">{t("status4")}</option>
-              <option value="COMPLETED">{t("status5")}</option>
-              <option value="MOVED ">{t("status6")}</option>
-            </select>
-          ) : (
-            <input
-              type="search"
-              placeholder={t("productNameSearch")}
-              defaultValue=""
-              onChange={searchProduct}
-              className="lg:w-4/12 ps-2 h-10 focus:outline-0 border sm:mt-0 mt-2"
-            />
-          )}
+          <input
+            type="search"
+            placeholder={t("productNameSearch")}
+            defaultValue=""
+            onChange={searchProduct}
+            className="lg:w-4/12 ps-2 h-10 focus:outline-0 border sm:mt-0 mt-2"
+          />
         </div>
       </div>
       <p className="mb-3">{t("cardCurrent")}: 1</p>
@@ -260,10 +194,7 @@ const DashboardProductCard = ({ className, lang }) => {
                   </td>
                   <td className="px-6 py-4">
                     <a
-                      onClick={() => {
-                        getInfo(item.id)
-                        openModal()
-                      }}
+                      onClick={() => getInfo(item.id)}
                       href="#"
                       className="font-medium text-[#16A34A] hover:underline"
                     >
